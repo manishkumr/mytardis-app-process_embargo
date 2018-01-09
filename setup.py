dcf4410b97d8759bbf2ede27ef79a697c14f410e
manishkumr <rishimanish123@gmail.com>
2018-01-09 14:39:23 +1100

    changed app name

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 version = '0.0.1'
 
-setup(name='anstoProcessEmbargo',
+setup(name='process_embargo',
       version=version,
       description="MyTardis app to process embargo",
       long_description="""\
